25290e81eb4324332e63c5e0152dc30e9800a5ed
OldMius <xbibol@gmail.com>
2016-02-10 23:19:12 +0100

    Dzałająca wersja po lekcji 4, tchnięte życie :p

--- app/src/main/java/pl/oldmius/todoekspert/LoginActivity.java
+++ app/src/main/java/pl/oldmius/todoekspert/LoginActivity.java
@@ -3,15 +3,29 @@ package pl.oldmius.todoekspert;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        button = (Button) findViewById(R.id.login_btn);
+        button.setOnClickListener(this);
+        usernameEditText = (EditText) findViewById(R.id.username_et);
+        passwordEditText = (EditText) findViewById(R.id.password_et);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -38,5 +52,35 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        boolean isError = false;
+
+        if (TextUtils.isEmpty(username)) {
+            usernameEditText.setError(getString(R.string.this_field_is_required));
+            isError = true;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            passwordEditText.setError(getString(R.string.this_field_is_required));
+            isError = true;
+        }
+
+        if (!isError) {
+            login(username, password);
+        }
+    }
+
+    private void login(String username, String password) {
+        if (username.equals("test") && password.equals("test")) {
+            Toast.makeText(getApplicationContext(), R.string.login_ok, Toast.LENGTH_SHORT).show();
+
+        } else
+            Toast.makeText(getApplicationContext(), R.string.wrong_username_or_password, Toast.LENGTH_SHORT).show();
+
     }
 }
