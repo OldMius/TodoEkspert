af67e70adff5d32c301f5b53ac7b8b983dfa4d20
OldMius <xbibol@gmail.com>
2016-02-17 23:10:56 +0100

    Działająca wersja z dodanym trzecim activity.

--- app/src/main/java/pl/oldmius/todoekspert/AddTodoActivity.java
+++ app/src/main/java/pl/oldmius/todoekspert/AddTodoActivity.java
@@ -3,8 +3,6 @@ package pl.oldmius.todoekspert;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class AddTodoActivity extends AppCompatActivity {
 
@@ -12,28 +10,11 @@ public class AddTodoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_todo);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-
-        return super.onOptionsItemSelected(item);
-    }
 }
